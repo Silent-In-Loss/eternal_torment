8a83668384ce2ec8397780f7b14851b3a00cb73c
Silent-In-Loss <osanbedesanta@gmail.com>
2024-09-23 17:02:28 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const menu = document.querySelector('.menu')
 burger.onclick = () => {
     menu.classList.toggle('open')
 }
+
+const modalBtn = document.querySelector('#modalBtn')
+const closeBtn = document.querySelector('#closeBtn')
+const modal = document.querySelector('.modal')
+
+modalBtn.onclick = () => {
+    modal.classList.add('open-modal')
+}
+
+closeBtn.onclick = () => {
+    modal.classList.remove('open-modal')
+}
+
+modal.onclick = (e) => {
+    // modal.classList.remove('open-modal')
+    console.log(e.target);
+
+    if(!e.target.closest('.modal__box')) {
+        modal.classList.remove('open-modal')
+    }
+}
